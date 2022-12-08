cb96a1124950dad6b0512d3454910ba5ff9ebde8
reko5 <reichert.kornel@gmail.com>
2022-12-08 13:22:04 +0100

    Dev Users.js
    Table with sticky header added to Users.js from Material UI library. It shows user data (columns) First Name, Last Name, Created At with pagination. Every page display 10 users (can be choose 25 or 100 users per page too).

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import ReactDOM from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import { Route, Routes } from "react-router-dom";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 import UserList from "./components/Users";
 // import "./App.css";
 
 function App() {
   return (
     <>
-
-        <UserList/>
-
+      <UserList />
     </>
   );
 }
 
 export default App;
+
+/*
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/Users" element={<UserList />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+*/
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getUsers } from "../services/UsersService";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+const columns = [
+  { id: "first_name", label: "First Name", minWidth: 170 },
+  { id: "last_name", label: "Last Name", minWidth: 100 },
+  {
+    id: "created_at",
+    label: "Created At",
+    minWidth: 170,
+    /*align: 'right',
+    format: (value) => value.toLocaleString('en-US'),*/
+  },
+];
 
 export default function UserList() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -13,15 +35,63 @@ export default function UserList() {
     fetchUsers();
   }, []);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <>
-      <div>
-        {users.map((users) => (
-          <div key={users.id}>
-            {users.first_name}, {users.last_name}, {users.created_at}
-          </div>
-        ))}
-      </div>
-    </>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((users) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={users.id}>
+                    {columns.map((column) => {
+                      const value = users[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
